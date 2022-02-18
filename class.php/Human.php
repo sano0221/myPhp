8f9468d97f2ac3112d33970ae3ad12234367a85c
sano0221 <daichiktennis0221@icloud.com>
2022-02-19 01:28:36 +0900

    second

---
+++ class.php/Human.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+class Human
+
+{
+    public $name;
+    public $hitPoint;
+    public $attackPoint;
+    
+    public function _const()
+    {
+        
+    }
+}
